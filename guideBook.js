2fe0f926fc4dae6c493cbdb232d2a7d059da5ac6
kelvin.biffi <kelvin.biffi@pmweb.com.br>
2021-01-18 22:57:33 -0300

    Ajustado exemplos criando testes de exemplo

--- guideBook.js
+++ guideBook.js
@@ -1,9 +1,10 @@
-import { GuideBook } from 'C:/PROJETOS/Guide-Book/index.js';
+const { GuideBook } = require('C:/PROJETOS/Guide-Book/index.js');
 
 const settings = {
   style: 'build/style.min.css',
-  source: 'source/css'
+  source: 'source/css',
+  output: 'guidebook',
 }
 
-const guideBook = new GuideBook(pageBase);
+const guideBook = new GuideBook();
 guideBook.generate(settings);
